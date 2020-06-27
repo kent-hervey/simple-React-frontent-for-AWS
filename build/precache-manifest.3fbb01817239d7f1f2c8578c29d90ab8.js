94405b96886346609b4bba0e2e4254fd903df7e3
Kent Hervey <41525831+kent-hervey@users.noreply.github.com>
2020-06-27 12:46:09 -0500

    committing build

---
+++ build/precache-manifest.3fbb01817239d7f1f2c8578c29d90ab8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8aa5b0b773905702766a1d4fdda5ec95",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f673f48c1f4d90afa477",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "18c7b0ffd00f01dcce88",
+    "url": "/static/js/2.40bc37bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.40bc37bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f673f48c1f4d90afa477",
+    "url": "/static/js/main.1261f4d3.chunk.js"
+  },
+  {
+    "revision": "2abba3fce0ba1c256171",
+    "url": "/static/js/runtime-main.c3ab611a.js"
+  }
+]);
